b85a5df5828e1f2b45af57d8be63de8f79b3e7c7
Santosh Kumar <sntshkmr60@gmail.com>
2012-09-24 17:18:47 +0530

    made visual changes
    Following had been added/changed:
    
     1. "Nunito" font has been added form Google Font
        Directory
     2. Appearance of color names has been moved to
        center of each compartment.
     3. Animation effect added on hovering over each
        compartment.

--- colorator.php
+++ colorator.php
@@ -5,20 +5,20 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width">
-
-<?php
-// {{{
+        <link href='http://fonts.googleapis.com/css?family=Nunito:700' rel='stylesheet' type='text/css'>
+<?php // {{
     $rand = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
     $color1 = '#'.$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)];
     $color2 = '#'.$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)];
     $color3 = '#'.$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)];
     $color4 = '#'.$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)].$rand[rand(0,15)];
-// }}}
-?>
+// }}?>
 
         <style type="text/css"> 
             body, html { height: 100%; padding: 0; margin: 0; }
-            div { width: 50%; height: 50%; float: left; }
+            div { width: 50%; height: 50%; float: left; display: table; -webkit-transition: all 500ms; transition: all 500ms; }
+            div:hover { -o-box-shadow: inset 0 0 40px #000000; -ms-box-shadow: inset 0 0 40px #000000; -moz-box-shadow: inset 0 0 40px #000000; -webkit-box-shadow: inset 0 0 40px #000000; box-shadow: inset 0 0 40px #000000; }
+            h1 { display: table-cell; text-align: center; vertical-align: middle; font-family: 'Nunito', sans-serif; font-size: 3em; } 
             #div1 { background: <?php echo $color1; ?>; }
             #div2 { background: <?php echo $color2; ?>; }
             #div3 { background: <?php echo $color3; ?>; }
