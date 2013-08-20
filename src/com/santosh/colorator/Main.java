ccd4a7a3c2f8e95aeb08312dd387af51c8d39037
Santosh Kumar <sntshkmr60@gmail.com>
2013-08-20 08:33:17 +0530

    Add psuedo code to work on

--- src/com/santosh/colorator/Main.java
+++ src/com/santosh/colorator/Main.java
@@ -2,6 +2,8 @@ package com.santosh.colorator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class Main extends Activity
 {
@@ -12,4 +14,12 @@ public class Main extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    /* TODO:
+     * Something like on: public void orientationChange() {
+     *     super .orientationChange();
+     *     Toast show = Toast.makeText(this, "Orientation changed..", Toast.LENGTH_SHORT);
+     *     show.setGravity(Gravity.CENTER, 0, 0);
+     *     show.show();
+     * }
+     */
 }
