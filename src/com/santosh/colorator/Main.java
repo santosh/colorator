e2720e7975832436809958e398172c3ba6c2b958
Santosh Kumar <sntshkmr60@gmail.com>
2014-03-18 20:02:20 +0530

    removes unnecessary code

--- src/com/santosh/colorator/Main.java
+++ src/com/santosh/colorator/Main.java
@@ -2,8 +2,6 @@ package com.santosh.colorator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.Toast;
 
 public class Main extends Activity
 {
@@ -14,12 +12,4 @@ public class Main extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-    /* TODO:
-     * Something like on: public void orientationChange() {
-     *     super .orientationChange();
-     *     Toast show = Toast.makeText(this, "Orientation changed..", Toast.LENGTH_SHORT);
-     *     show.setGravity(Gravity.CENTER, 0, 0);
-     *     show.show();
-     * }
-     */
 }
