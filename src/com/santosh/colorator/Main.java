e217b283a0067a87cab0b58e8aeb064e6047de1e
Santosh Kumar <sntshkmr60@gmail.com>
2014-03-21 08:08:21 +0530

    adds OnClickListener
    ..to listen to on tap event

--- src/com/santosh/colorator/Main.java
+++ src/com/santosh/colorator/Main.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class Main extends Activity
 {
@@ -16,5 +18,13 @@ public class Main extends Activity
         setContentView(R.layout.main);
         TextView colorful = (TextView)findViewById(R.id.colorful);
         colorful.setBackgroundResource(R.color.white);
+        colorful.setOnClickListener(colorfullistener);
     }
+
+    private OnClickListener colorfullistener = new View.OnClickListener() {
+    @Override
+        public void onClick(View colorful) {
+            colorful.setBackgroundResource(R.color.blue);
+        }
+    };
 }
