e3192371474396d86e984201e3d0c6472c48f23c
Santosh Kumar <sntshkmr60@gmail.com>
2014-03-21 07:10:27 +0530

    set default color to white
    ..when app opens

--- src/com/santosh/colorator/Main.java
+++ src/com/santosh/colorator/Main.java
@@ -3,6 +3,7 @@ package com.santosh.colorator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 public class Main extends Activity
 {
@@ -13,5 +14,7 @@ public class Main extends Activity
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        TextView colorful = (TextView)findViewById(R.id.colorful);
+        colorful.setBackgroundResource(R.color.white);
     }
 }
