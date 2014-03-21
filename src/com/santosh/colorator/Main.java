ec22b3186e8dacfc6a9c151e0a0d3eacebc83c6b
Santosh Kumar <sntshkmr60@gmail.com>
2014-03-21 08:14:59 +0530

    defines colorful TextView outside onCreate
    ..to make it accessible to OnClickListener

--- src/com/santosh/colorator/Main.java
+++ src/com/santosh/colorator/Main.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 public class Main extends Activity
 {
+    TextView colorful;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -16,7 +17,7 @@ public class Main extends Activity
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-        TextView colorful = (TextView)findViewById(R.id.colorful);
+        colorful = (TextView)findViewById(R.id.colorful);
         colorful.setBackgroundResource(R.color.white);
         colorful.setOnClickListener(colorfullistener);
     }
